b030077903bf0b266a411e19e2ee68098e1ee32a
TedWildenradt <tedkwildenradt@gmail.com>
2019-03-14 12:38:34 -0700

    Set tokens on local storage

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -26,8 +26,9 @@ class Signup extends React.Component {
 
   handleSubmit = ( event, signupUser) => {
     event.preventDefault();
-    signupUser().then(data => {
+    signupUser().then(({data}) => {
       console.log(data)
+      localStorage.setItem('token', data.signupUser.token);
       this.clearState();
     })
   }
