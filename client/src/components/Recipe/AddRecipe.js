8e2f90cba6bac88aa86032f772479679d175decc
TedWildenradt <tedkwildenradt@gmail.com>
2019-03-15 14:02:31 -0700

    Fix render error in addRecipe

--- client/src/components/Recipe/AddRecipe.js
+++ client/src/components/Recipe/AddRecipe.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import {Mutation} from 'react-apollo';
+import {ADD_RECIPE} from '../../queries/index';
+import Error from '../error';
+
 class AddRecipe extends React.Component {
   state = {
     name: '',
@@ -10,6 +14,12 @@ class AddRecipe extends React.Component {
 
   }
 
+  componentDidMount(){
+    this.setState({
+      username: this.props.session.getCurrentUser.username
+    })
+  }
+
   handleChange = event => {
     const {name, value,} = event.target;
     this.setState({
@@ -17,12 +27,29 @@ class AddRecipe extends React.Component {
     })
   }
 
+  validateForm = () => {
+    const {name,category,description,instructions, username} = this.state
+    const isInvalid = !name || !category || !description || !instructions;
+    return isInvalid;
+  }
+
+  handleSubmit = (event, addRecipe) => {
+    event.preventDefault();
+    addRecipe().then(({data}) => {
+      console.log(data)
+    })
+  }
+
   render(){
-    const {name,category,description,instructions,} = this.state
+    const {name,category,description,instructions, username} = this.state
     return(
+    <Mutation mutation={ADD_RECIPE} variables={{name,category,description,instructions, username}}>
+    {(addRecipe, {data, loading, error}) => {
+
+      return(
       <div className="App">
         <h2 className="App">Add Recipe</h2>
-        <form className="form">
+        <form className="form" onSubmit={(event) => this.handleSubmit(event, addRecipe)}>
           <input type="text" name="name" onChange={this.handleChange} placeholder="Recipe Name" value={name}/>
           <select name="category" onChange={this.handleChange} value={category}>
             <option value="Breakfast">Breakfast</option>
@@ -32,9 +59,14 @@ class AddRecipe extends React.Component {
           </select>
           <input type="text" name="description" onChange={this.handleChange} placeholder="Add Description" value={description}/>
           <textarea name="instructions" onChange={this.handleChange} placeholder="Add Instructions" value={instructions}/>
-          <button className="button-primary">Submit</button>
+          <button className="button-primary" disabled={loading || this.validateForm()}>Submit</button>
+          {error && <Error error={error} />}
         </form>
       </div>
+      )
+
+    }}
+    </Mutation>
     )
   }
 }
--- client/src/queries/index.js
+++ client/src/queries/index.js
@@ -25,6 +25,21 @@ export const GET_RECIPE = gql `
   }
 `;
 
+// Recipe Mutations
+export const ADD_RECIPE = gql`
+  mutation($name: String!, $description: String!, $category: String!, $instructions: String!, $username: String){
+    addRecipe(name: $name, description: $description, category: $category, instructions: $instructions, username: $username ) {
+      _id
+      name
+      category
+      description
+      instructions
+      createdDate
+      likes
+    }
+  }
+`;
+
 // User Queries
 export const GET_CURRENT_USER = gql`
   query {
