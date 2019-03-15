aa9a440edae7a0fd84de89c9c5472e90063e141e
TedWildenradt <tedkwildenradt@gmail.com>
2019-03-15 13:33:02 -0700

    Create addRecipe component with form

--- client/src/components/Recipe/AddRecipe.js
+++ client/src/components/Recipe/AddRecipe.js
@@ -1,9 +1,42 @@
 import React from 'react';
 
-const AddRecipe = () => (
-  <div>
-    Add Recipe
-  </div>
-)
+class AddRecipe extends React.Component {
+  state = {
+    name: '',
+    instructions: '',
+    category: 'Breakfast',
+    description: '',
+    username: ''
+
+  }
+
+  handleChange = event => {
+    const {name, value,} = event.target;
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  render(){
+    const {name,category,description,instructions,} = this.state
+    return(
+      <div className="App">
+        <h2 className="App">Add Recipe</h2>
+        <form className="form">
+          <input type="text" name="name" onChange={this.handleChange} placeholder="Recipe Name" value={name}/>
+          <select name="category" onChange={this.handleChange} value={category}>
+            <option value="Breakfast">Breakfast</option>
+            <option value="Lunch">Lunch</option>
+            <option value="Dinner">Dinner</option>
+            <option value="Snack">Snack</option>
+          </select>
+          <input type="text" name="description" onChange={this.handleChange} placeholder="Add Description" value={description}/>
+          <textarea name="instructions" onChange={this.handleChange} placeholder="Add Instructions" value={instructions}/>
+          <button className="button-primary">Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
 
 export default AddRecipe;
