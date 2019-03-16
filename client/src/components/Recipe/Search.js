b8fa77295cb2012b4351fc2c6f8d4c70f8430b72
TedWildenradt <tedkwildenradt@gmail.com>
2019-03-16 10:49:51 -0700

    Fix topology error

--- client/src/components/Recipe/Search.js
+++ client/src/components/Recipe/Search.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import {Query } from 'react-apollo';
+import {SEARCH_RECIPES} from '../../queries';
+
 const Search = () => (
-  <div>Search</div>
+  <Query query={SEARCH_RECIPES} variables={{ searchTerm: '' }}>
+    {({ data, loading, error}) => {
+      if (loading) return <div>Loading</div>;
+      if (error) return <div>Error</div>;
+      console.log(data)
+
+       return (
+        <div className="App">
+          <input type="search" />
+        </div>
+       )
+    }}
+  </Query>
 )
 
 export default Search;
