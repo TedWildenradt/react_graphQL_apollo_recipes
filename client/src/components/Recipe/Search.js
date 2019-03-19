05dc3b250c07df6e02714fb68b142f54adab5082
TedWildenradt <tedkwildenradt@gmail.com>
2019-03-19 10:15:33 -0700

    Search results now work

--- client/src/components/Recipe/Search.js
+++ client/src/components/Recipe/Search.js
@@ -4,30 +4,47 @@ import {Link} from 'react-router-dom';
 import {ApolloConsumer } from 'react-apollo';
 import {SEARCH_RECIPES} from '../../queries';
 
-const Search = () => (
-  <Query query={SEARCH_RECIPES} variables={{ searchTerm: '' }}>
-    {({ data, loading, error}) => {
-      if (loading) return <div>Loading</div>;
-      if (error) return <div>Error</div>;
-      console.log(data)
+class Search extends React.Component{
 
-       return (
-        <div className="App">
-          <input type="search" />
+  state = {
+    searchResults: [] 
+  }
+  handleChange = ({searchRecipes}) => {
+    this.setState({
+      searchResults: searchRecipes
+    })
+  }
+
+  render(){
+    const {searchResults} = this.state;
+    return(
+      <ApolloConsumer>
+        {client => (
+          <div className="App">
+          <input type="search" placeholder="Search for Recipes" onChange={ async (event) => {
+            event.persist();
+            const {data} = await client.query({
+              query: SEARCH_RECIPES,
+              variables: {searchTerm: event.target.value}
+            });
+            this.handleChange(data);
+          }}/>
           <ul>
-            {data.searchRecipes.map(recipe => {
+            {searchResults.map(recipe => {
               return(
                 <li key={recipe._id}>
                   <Link to={`/recipes/${recipe._id}`}><h4>{recipe.name}</h4></Link>
-                  <p>{recipe.likes}</p>
+                  <p>Likes: {recipe.likes}</p>
                 </li>
               )
             })}
           </ul>
         </div>
-       )
-    }}
-  </Query>
-)
+        )}
+      </ApolloConsumer>
+    )
+  }
+}
+
 
 export default Search; 
